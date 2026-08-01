82ab5031026fdd10f3fcb53f8d2ace6cc48f5ccc
martinsAkin <martinsakin98@gmail.com>
2026-08-01 17:38:18 +0100

    updated backend server url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3198/api",
+  baseURL: `${import.meta.env.VITE_API_URL || ""}/api`,
 });
 
 // attach the session token to every outgoing request, if we have one
